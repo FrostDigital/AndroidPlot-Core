68d619fe463c1ab346740d97340fa74080c12a80
manneohlund <manne@frostdigital.se>
2017-02-22 14:59:07 +0100

    [FIX] Unit tests with mock and mock context

--- src/test/java/com/androidplot/mock/MockContext.java
+++ src/test/java/com/androidplot/mock/MockContext.java
@@ -16,25 +16,40 @@
 
 package com.androidplot.mock;
 
-import android.content.*;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
+import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.content.res.AssetManager;
+import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Bitmap;
-import android.graphics.Paint;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
-import mockit.Instantiation;
+import android.os.UserHandle;
+import android.view.Display;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 import mockit.Mock;
 import mockit.MockClass;
-
-import java.io.*;
 
 @MockClass(realClass = Context.class)
 public class MockContext {
@@ -141,8 +156,18 @@ public class MockContext {
         }
 
         @Override
+        public File getNoBackupFilesDir() {
+            return null;
+        }
+
+        @Override
         public File getExternalFilesDir(String s) {
             return null;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public File[] getExternalFilesDirs(String s) {
+            return new File[0];
         }
 
         @Override
@@ -151,13 +176,33 @@ public class MockContext {
         }
 
         @Override
+        public File[] getObbDirs() {
+            return new File[0];
+        }
+
+        @Override
         public File getCacheDir() {
             return null;  //To change body of implemented methods use File | Settings | File Templates.
         }
 
         @Override
+        public File getCodeCacheDir() {
+            return null;
+        }
+
+        @Override
         public File getExternalCacheDir() {
             return null;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public File[] getExternalCacheDirs() {
+            return new File[0];
+        }
+
+        @Override
+        public File[] getExternalMediaDirs() {
+            return new File[0];
         }
 
         @Override
@@ -281,6 +326,21 @@ public class MockContext {
         }
 
         @Override
+        public void sendBroadcastAsUser(Intent intent, UserHandle userHandle) {
+
+        }
+
+        @Override
+        public void sendBroadcastAsUser(Intent intent, UserHandle userHandle, String s) {
+
+        }
+
+        @Override
+        public void sendOrderedBroadcastAsUser(Intent intent, UserHandle userHandle, String s, BroadcastReceiver broadcastReceiver, Handler handler, int i, String s1, Bundle bundle) {
+
+        }
+
+        @Override
         public void sendStickyBroadcast(Intent intent) {
             //To change body of implemented methods use File | Settings | File Templates.
         }
@@ -293,6 +353,21 @@ public class MockContext {
         @Override
         public void removeStickyBroadcast(Intent intent) {
             //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public void sendStickyBroadcastAsUser(Intent intent, UserHandle userHandle) {
+
+        }
+
+        @Override
+        public void sendStickyOrderedBroadcastAsUser(Intent intent, UserHandle userHandle, BroadcastReceiver broadcastReceiver, Handler handler, int i, String s, Bundle bundle) {
+
+        }
+
+        @Override
+        public void removeStickyBroadcastAsUser(Intent intent, UserHandle userHandle) {
+
         }
 
         @Override
@@ -423,6 +498,16 @@ public class MockContext {
         @Override
         public Context createPackageContext(String s, int i) throws PackageManager.NameNotFoundException {
             return null;  //To change body of implemented methods use File | Settings | File Templates.
+        }
+
+        @Override
+        public Context createConfigurationContext(Configuration configuration) {
+            return null;
+        }
+
+        @Override
+        public Context createDisplayContext(Display display) {
+            return null;
         }
     }
 }
